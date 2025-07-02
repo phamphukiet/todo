637fd59192526a8ecabfaa379c65580563e0b460
Alalp <80088308+0858433409@users.noreply.github.com>
2025-07-02 21:14:05 +0700

    Push code mới, bỏ qua node_modules

--- todo-app/backend/db/index.js
+++ todo-app/backend/db/index.js
@@ -5,10 +5,20 @@ const pool = new Pool({
   user: "postgres",
   host: "localhost",
   database: "todoapp",
-  password: "12345",
+  password: "123456",
   //   database: "todo_app", // CSDL trong pg
   //   password: "your_password", // Pass trong pg
   port: 5432,
 });
 
+(async () => {
+  try {
+    const res = await pool.query('SELECT NOW()');
+    console.log("Kết nối thành công! Time:", res.rows[0]);
+  } catch (err) {
+    console.error("❌ Lỗi kết nối:", err.message);
+  } finally {
+    pool.end();
+  }
+})();
 module.exports = pool;
