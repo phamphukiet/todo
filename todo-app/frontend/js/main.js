0f5a9c5859b5a9b8baafb05d1fe90c2240b4385a
Alalp <80088308+0858433409@users.noreply.github.com>
2025-07-04 02:48:56 +0700

    Cập nhật giao diện login, register, css, js

--- todo-app/frontend/js/main.js
+++ todo-app/frontend/js/main.js
@@ -181,3 +181,90 @@ document.addEventListener("DOMContentLoaded", () => {
     window.location.href = "../user/login.html";
   });
 });
+//  code này là phần nâng cấp giao diện
+// function togglePassword() { 
+//   const pwd = document.getElementById("password");
+//   const icon = document.querySelector(".toggle-password");
+//   if (pwd.type === "password") {
+//     pwd.type = "text";
+//     icon.classList.remove("fa-eye");
+//     icon.classList.add("fa-eye-slash");
+//   } else {
+//     pwd.type = "password";
+//     icon.classList.remove("fa-eye-slash");
+//     icon.classList.add("fa-eye");
+//   }
+// }
+
+// let currentLang = "vie";
+// function toggleLanguage() {
+//   const slider = document.getElementById("langSlider");
+//   if (currentLang === "vie") {
+//     slider.style.transform = "translateX(42px)";
+//     currentLang = "eng";
+//     applyEnglish();
+//   } else {
+//     slider.style.transform = "translateX(0)";
+//     currentLang = "vie";
+//     applyVietnamese();
+//   }
+// }
+
+// function applyEnglish() {
+//   document.getElementById("siteSubtitle").innerText = "Build a To-Do List management website";
+//   document.getElementById("loginTitle").innerText = "Login";
+//   document.getElementById("username").placeholder = "Username";
+//   document.getElementById("password").placeholder = "Password";
+//   document.getElementById("loginButton").innerText = "Login";
+//   document.getElementById("noAccount").innerText = "Don't have an account?";
+//   document.getElementById("registerLink").innerText = "Register here";
+// }
+
+// function applyVietnamese() {
+//   document.getElementById("siteSubtitle").innerText = "Xây dựng website quản lý To-Do List";
+//   document.getElementById("loginTitle").innerText = "Đăng nhập";
+//   document.getElementById("username").placeholder = "Tên đăng nhập";
+//   document.getElementById("password").placeholder = "Mật khẩu";
+//   document.getElementById("loginButton").innerText = "Đăng nhập";
+//   document.getElementById("noAccount").innerText = "Bạn chưa có tài khoản?";
+//   document.getElementById("registerLink").innerText = "Đăng ký tại đây";
+// }
+// function applyEnglish() {
+//   if(document.getElementById("loginTitle")) {
+//     document.getElementById("siteSubtitle").innerText = "Build a To-Do List management website";
+//     document.getElementById("loginTitle").innerText = "Login";
+//     document.getElementById("username").placeholder = "Username";
+//     document.getElementById("password").placeholder = "Password";
+//     document.getElementById("loginButton").innerText = "Login";
+//     document.getElementById("noAccount").innerText = "Don't have an account?";
+//     document.getElementById("registerLink").innerText = "Register here";
+//   } else if(document.getElementById("registerTitle")) {
+//     document.getElementById("siteSubtitle").innerText = "Build a To-Do List management website";
+//     document.getElementById("registerTitle").innerText = "Register";
+//     document.getElementById("username").placeholder = "Username";
+//     document.getElementById("password").placeholder = "Password";
+//     document.getElementById("registerButton").innerText = "Create account";
+//     document.getElementById("hasAccount").innerText = "Already have an account?";
+//     document.getElementById("loginLink").innerText = "Login";
+//   }
+// }
+
+// function applyVietnamese() {
+//   if(document.getElementById("loginTitle")) {
+//     document.getElementById("siteSubtitle").innerText = "Xây dựng website quản lý To-Do List";
+//     document.getElementById("loginTitle").innerText = "Đăng nhập";
+//     document.getElementById("username").placeholder = "Tên đăng nhập";
+//     document.getElementById("password").placeholder = "Mật khẩu";
+//     document.getElementById("loginButton").innerText = "Đăng nhập";
+//     document.getElementById("noAccount").innerText = "Bạn chưa có tài khoản?";
+//     document.getElementById("registerLink").innerText = "Đăng ký tại đây";
+//   } else if(document.getElementById("registerTitle")) {
+//     document.getElementById("siteSubtitle").innerText = "Xây dựng website quản lý To-Do List";
+//     document.getElementById("registerTitle").innerText = "Đăng ký";
+//     document.getElementById("username").placeholder = "Tên đăng nhập";
+//     document.getElementById("password").placeholder = "Mật khẩu";
+//     document.getElementById("registerButton").innerText = "Tạo tài khoản";
+//     document.getElementById("hasAccount").innerText = "Đã có tài khoản?";
+//     document.getElementById("loginLink").innerText = "Đăng nhập";
+//   }
+// }
