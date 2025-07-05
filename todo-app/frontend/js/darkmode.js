0ccc696cb814df462512f5c89fc3bd04f3787769
KhanhSun <2224802010480@student.tdmu.edu.vn>
2025-07-05 10:18:59 +0700

    update 0.0.0.2

--- todo-app/frontend/js/darkmode.js
+++ todo-app/frontend/js/darkmode.js
@@ -4,10 +4,16 @@ const body = document.querySelector('body'),
     modeSwitch = body.querySelector('.toggle-switch'),
     modeText = body.querySelector('.mode-text');
 
-modeSwitch.addEventListener("click", () => {
-    body.classList.toggle("close");
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
 });
 
 modeSwitch.addEventListener("click", () => {
     body.classList.toggle("dark");
+
+    if (body.classList.contains("dark")) {
+        modeText.innerText = "Light Mode";
+    } else {
+        modeText.innerText = "Dark Mode";
+    }
 });
