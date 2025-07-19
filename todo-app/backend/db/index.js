37196a84b5ac7de985fe14d32faaff86d129e778
2224802010569 <2224802010569@student.tdmu.edu.vn>
2025-07-19 10:19:02 +0700

    Revert "0.13 render"
    This reverts commit 88c3b26d369c66128d16067f965a53d08519eb6a.

--- todo-app/backend/db/index.js
+++ todo-app/backend/db/index.js
@@ -1,24 +1,17 @@
 // Kết nối PostgreSQL
 const { Pool } = require("pg");
 
-// const pool = new Pool({
-//   user: "postgres",
-//   host: "localhost",
-//   //database: "todoapp",
-//   //password: "12345",
-//   //   database: "todo_app", // CSDL trong pg
-//   //   password: "your_password", // Pass trong pg
-//   database: "Cloud To-do",
-//   password: "khanhanh12",
-
-//   port: 5432,
-// });
-
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  user: "postgres",
+  host: "localhost",
+  //database: "todoapp",
+  //password: "12345",
+  //   database: "todo_app", // CSDL trong pg
+  //   password: "your_password", // Pass trong pg
+  database: "Cloud To-do",
+  password: "khanhanh12",
+
+  port: 5432,
 });
 
 module.exports = pool;
