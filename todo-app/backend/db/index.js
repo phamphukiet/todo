5237cfaf590bb835e860b80196ed87246a7aeebd
2224802010569 <2224802010569@student.tdmu.edu.vn>
2025-07-19 10:08:27 +0700

    0.13 render

--- todo-app/backend/db/index.js
+++ todo-app/backend/db/index.js
@@ -1,17 +1,24 @@
 // Kết nối PostgreSQL
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  //database: "todoapp",
-  //password: "12345",
-  //   database: "todo_app", // CSDL trong pg
-  //   password: "your_password", // Pass trong pg
-  database: "Cloud To-do",
-  password: "khanhanh12",
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   //database: "todoapp",
+//   //password: "12345",
+//   //   database: "todo_app", // CSDL trong pg
+//   //   password: "your_password", // Pass trong pg
+//   database: "Cloud To-do",
+//   password: "khanhanh12",
 
-  port: 5432,
+//   port: 5432,
+// });
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
